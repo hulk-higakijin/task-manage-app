a44bdb2f137e8be4ecc133b0142da172d3bf7f94
higakijin <hm385.chejptks@gmail.com>
2021-09-26 10:38:06 +0900

    add validates

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,15 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+  validates :name, length: { maximum: 30 }, presence: true
+  validate :validate_name_not_including_commma
+
+  private
   
+    def validate_name_not_including_commma
+      errors.add(:name, 'にカンマを含めることはできません。') if name&.include?(',')
+    end
+
+    def set_nameless_name
+      self.name = '名前なし', if name.blank?
+    end
 end
